a5feeff5c9c208672d59cd70bda3156f3288672f
Doug Turnbull <softwaredoug@gmail.com>
2019-01-03 15:49:38 -0500

    Add stuff for custom qparser examples

--- indexTmdb.py
+++ indexTmdb.py
@@ -12,6 +12,9 @@ def indexableMovies():
             if 'release_date' in tmdbMovie and len(tmdbMovie['release_date']) > 0:
                 releaseDate = tmdbMovie['release_date'] + 'T00:00:00Z'
 
+            if movieId == '374430':
+                print(tmdbMovie['vote_count'])
+
             yield {'id': movieId,
                    'title': tmdbMovie['title'],
                    'overview': tmdbMovie['overview'],
@@ -20,8 +23,8 @@ def indexableMovies():
                    'cast': [castMember['name'] for castMember in tmdbMovie['cast']],
                    'genres': [genre['name'] for genre in tmdbMovie['genres']],
                    'release_date': releaseDate,
-                   'vote_average': tmdbMovie['vote_average'] if 'vote_average' in tmdbMovie else None,
-                   'vote_count': int(tmdbMovie['vote_count']) if 'vote_count' in tmdbMovie else None,
+                   'vote_average': float(tmdbMovie['vote_average']) if 'vote_average' in tmdbMovie else None,
+                   'vote_count': int(tmdbMovie['vote_count']) if 'vote_count' in tmdbMovie else 0,
                    }
         except KeyError as k: # Ignore any movies missing these attributes
             print(k)
