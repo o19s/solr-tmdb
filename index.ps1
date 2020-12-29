a2f513f7ffb517c9283648c7f0d60c0d6c8856d4
Nathan Day <nathancday@gmail.com>
2020-12-29 14:26:47 -0500

    update index script to actually wipe an existing index

--- index.ps1
+++ index.ps1
@@ -1,4 +1,10 @@
 # Powershell script for Windows 10 and above to index the TMDB JSON file.
 
+# delete (or wipe) the tmdb index and reindex
+Invoke-WebRequest -Method POST -Uri 'http://localhost:8983/solr/tmdb/update' -Body '<delete><query>*:*</query></delete>' -Content-type 'text/xml; charset=utf-8'
+
+Invoke-WebRequest -Method POST -Uri 'http://localhost:8983/solr/tmdb/update' -Body '<commit/>' -ContentType 'text/xml; charset=utf-8'
+
+
 $data = Get-Content 'tmdb_solr_file.json'
 Invoke-WebRequest -Method POST -Uri 'http://localhost:8983/solr/tmdb/update' -ContentType 'application/json' -UseBasicParsing -Body $data
--- index.sh
+++ index.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# delete (or wipe) the tmdb index and reindex
+curl http://localhost:8983/solr/tmdb/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
+
+curl http://localhost:8983/solr/tmdb/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
+
 curl 'http://localhost:8983/solr/tmdb/update?commit=true' --data-binary @tmdb_solr.json -H 'Content-type:application/json'
