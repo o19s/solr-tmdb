d05c846d43a5ddc886181c2cddc65651f7e01520
Nathan Day <nathancday@gmail.com>
2021-02-02 08:42:01 -0500

    unzip logic to index.sh

--- index.sh
+++ index.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ ! -f ./tmdb_solr.json ]; then
+  unzip tmdb_solr.json.zip
+fi
 
 # delete (or wipe) the tmdb index and reindex
 curl http://localhost:8983/solr/tmdb/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
