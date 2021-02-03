7b841dc1314dc430b1910e0668ba5f42f2ba2c2a
Nathan (Nate) Day <nathancday@gmail.com>
2021-02-02 19:00:08 -0500

    new data (#20)
    * new data
    
    * update index script to actually wipe an existing index
    
    * Powershell fixes
    
    * unzip logic to index.sh
    
    Co-authored-by: = <danworley@gmail.com>

--- index.ps1
+++ index.ps1
@@ -1,4 +1,15 @@
 # Powershell script for Windows 10 and above to index the TMDB JSON file.
 
-$data = Get-Content 'tmdb_solr_file.json'
+if (![System.IO.File]::Exists('tmdb_data/tmdb_solr.json')) {
+  echo 'Extracting TMDB archive'
+  Expand-Archive  -DestinationPath tmdb_data 'tmdb_solr.json.zip'
+}
+
+# delete (or wipe) the tmdb index and reindex
+Invoke-WebRequest -Method POST -Uri 'http://localhost:8983/solr/tmdb/update' -Body '<delete><query>*:*</query></delete>' -ContentType 'text/xml; charset=utf-8'
+
+Invoke-WebRequest -Method POST -Uri 'http://localhost:8983/solr/tmdb/update' -Body '<commit/>' -ContentType 'text/xml; charset=utf-8'
+
+
+$data = Get-Content 'tmdb_data/tmdb_solr.json'
 Invoke-WebRequest -Method POST -Uri 'http://localhost:8983/solr/tmdb/update' -ContentType 'application/json' -UseBasicParsing -Body $data
--- index.sh
+++ index.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+if [ ! -f ./tmdb_solr.json ]; then
+  unzip tmdb_solr.json.zip
+fi
+
+# delete (or wipe) the tmdb index and reindex
+curl http://localhost:8983/solr/tmdb/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
+
+curl http://localhost:8983/solr/tmdb/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
 
 curl 'http://localhost:8983/solr/tmdb/update?commit=true' --data-binary @tmdb_solr.json -H 'Content-type:application/json'
