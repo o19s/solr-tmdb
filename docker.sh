5cd7f7f09f753cdd11f2c0e9160502eb8b9d51f8
Martin Knoller Stocker <linuxswords@gmail.com>
2022-01-27 08:57:58 +0100

    docker start linux: fix volume mount, use PWD variable, not command

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 sudo chown -R 8983:8983 solr_home   # necessary on Linux, not Mac according to https://github.com/docker-solr/docker-solr/blob/master/README.md
-docker run -p 8983:8983 -v $(PWD)/solr_home:/opt/mysolrhome -e SOLR_HOME=/opt/mysolrhome -e INIT_SOLR_HOME=yes solr:8.4.1
+docker run -p 8983:8983 -v ${PWD}/solr_home:/opt/mysolrhome -e SOLR_HOME=/opt/mysolrhome -e INIT_SOLR_HOME=yes solr:8.4.1
