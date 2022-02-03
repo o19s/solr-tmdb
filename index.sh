8be15df92b6f310858bea8f89259cbc57091e372
Jeff Zemerick <jzemerick@opensourceconnections.com>
2022-02-03 15:28:25 -0500

    Parameterizing the hostname for indexing and defaulting to localhost. (#24)
    Co-authored-by: Kevin Risden <krisden@opensourceconnections.com>

--- index.sh
+++ index.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+
+HOSTNAME=${1:-localhost}
+
 if [ ! -f ./tmdb_solr.json ]; then
   unzip tmdb_solr.json.zip
 fi
 
 # delete (or wipe) the tmdb index and reindex
-curl http://localhost:8983/solr/tmdb/update --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
+curl "http://$HOSTNAME:8983/solr/tmdb/update" --data '<delete><query>*:*</query></delete>' -H 'Content-type:text/xml; charset=utf-8'
 
-curl http://localhost:8983/solr/tmdb/update --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
+curl "http://$HOSTNAME:8983/solr/tmdb/update" --data '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
 
-curl 'http://localhost:8983/solr/tmdb/update?commit=true' --data-binary @tmdb_solr.json -H 'Content-type:application/json'
+curl "http://$HOSTNAME:8983/solr/tmdb/update?commit=true" --data-binary @tmdb_solr.json -H 'Content-type:application/json'
